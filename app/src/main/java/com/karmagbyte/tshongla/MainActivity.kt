1a06cb53fe1a2ed8abe4550d58978f1dbed5ae74
KarmaG-byte <02240155.cst@rub.edu.bt>
2026-09-07 21:20:21 +0530

    Fix ContentScale import directly

--- app/src/main/java/com/karmagbyte/tshongla/MainActivity.kt
+++ app/src/main/java/com/karmagbyte/tshongla/MainActivity.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ContentScale
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
